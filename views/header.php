65098a68a9125f9a7b7b40cf03af9961f6cf814f
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-16 19:21:05 -0400

    fixed it

--- views/header.php
+++ views/header.php
@@ -13,7 +13,18 @@
 
 </head>
 <body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=1766483266905544";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 	<h2> Gift List You </h2>
+<nav>
 <a href="?action=signup">Sign up</a>
 <a href="?action=logout">Logout</a>
+</nav>
 <br>
