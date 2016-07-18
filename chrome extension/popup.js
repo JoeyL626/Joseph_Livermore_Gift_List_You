ac83fc9162e6745275c5518e0698a3fa620b3e09
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-18 00:32:24 -0400

    chrome fix

--- chrome extension/popup.js
+++ chrome extension/popup.js
@@ -1,7 +1,3 @@
-$(document).ready(function(){
-	chrome.tabs.getSelected(null, function(tab){
-		var link = document.createElement('a');
-		link.href = tab.url
-		$('#host').html("Host: "+link.hostname);
-	})
+chrome.tabs.getSelected(null, function(tab) {
+    document.getElementById('host').innerHTML = tab.url;
 });
