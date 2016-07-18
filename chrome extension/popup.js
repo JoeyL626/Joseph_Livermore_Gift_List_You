fff9cea459417d41ed4e133fb89572c7d91fa48f
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-17 21:30:50 -0400

    chrome stuff

--- chrome extension/popup.js
+++ chrome extension/popup.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	chrome.tabs.getSelected(null, function(tab){
+	chrome.tabs.getCurrent(null, function(tab){
 		var link = document.createElement('a');
 		link.href = tab.url
 		$('#host').html(link.hostname);
