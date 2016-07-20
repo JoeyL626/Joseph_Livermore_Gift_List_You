e513f948f653e69aca81910f17be2d649b324fce
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 14:51:49 -0400

    style

--- views/list_view.php
+++ views/list_view.php
@@ -2,7 +2,7 @@
 <div class="small-5 columns float-right">
 <table >
 <th width="150">Event</th>
-<th width="300">Address</th>
+<th width="500">Address</th>
 <th width="150">Edit</th>
 <th width="150">Items</th>
 <?
