9d2a2249a97dbe08e2e8e878eb30479b74c76e6b
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 16:22:44 -0400

    style

--- views/user_profile.php
+++ views/user_profile.php
@@ -12,19 +12,7 @@
 </tr>
 <tr>
 <th>Address</th>
-<td><? echo $data[0][0]["address"]?></td>
-</tr>
-<tr>
-<th>City</th>
-<td><? echo $data[0][0]["city"]?></td>
-</tr>
-<tr>
-<th>State</th>
-<td><? echo $data[0][0]["state"]?></td>
-</tr>
-<tr>
-<th>Zip</th>
-<td><? echo $data[0][0]["zip"]?></td>
+<td><? echo $data[0][0]["address"]." ".$data[0][0]["city"].", ".$data[0][0]["state"]." ".$data[0][0]["zip"]?></td>
 </tr>
 </table>
 </div>
