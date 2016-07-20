7a4cfbbb1aa5b4cab3ad41daf430ca47500145d1
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 04:21:37 -0400

    even more login grid

--- views/user_login.php
+++ views/user_login.php
@@ -1,7 +1,7 @@
 
 <div class="row">
-<div class="small-2 columns"></div>
-<div class="small-8 columns">
+<div class="large-2 columns"></div>
+<div class="large-8 columns">
 <form enctype="multipart/form-data" action="?action=loginAction" method="POST">
 	<fieldset>
 
@@ -14,5 +14,5 @@
   </fieldset>
 </form>
 </div>
-<div class="small-2 columns"></div>
+<div class="large-2 columns"></div>
 </div>
