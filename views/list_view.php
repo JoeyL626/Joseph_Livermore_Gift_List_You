2a6c30ea2bbc71b22fbaecc83e0092599556b6b9
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 13:40:54 -0400

    style stuff

--- views/list_view.php
+++ views/list_view.php
@@ -5,15 +5,16 @@
 foreach($data[1] as $list){
 ?>
 <tr>
+<a href="?action=itemView&id=<? echo $list['list_id']?>">
 <th align=left><? echo $list["name"]?></th>
 <th align=left><? echo $list["event"]?></th>
 <th align=left><? echo $list["address"]?></th>
 <th align=left><? echo $list["city"]?></th>
 <th align=left><? echo $list["state"]?></th>
 <th align=left><? echo $list["zip"]?></th>
-<th align=left><a href="?action=updateListButton&id=<? echo $list['list_id']?>">Edit List</a></th>
-<th align=left><a href="?action=deleteListAction&id=<? echo $list['list_id']?>">Delete List</a></th>
-<th align=left><a href="?action=itemView&id=<? echo $list['list_id']?>">View Items</a></th>
+<th align=left ><a class="button" href="?action=updateListButton&id=<? echo $list['list_id']?>">Edit List</a></th>
+<th align=left><a class="button" href="?action=deleteListAction&id=<? echo $list['list_id']?>">Delete List</a></th>
+</a>
 </tr>
 <?
 }
