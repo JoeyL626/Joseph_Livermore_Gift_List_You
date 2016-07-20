593870153b7a602ee682b243eafd595811d2a404
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 13:46:46 -0400

    style stuff

--- views/list_view.php
+++ views/list_view.php
@@ -4,7 +4,6 @@
 <?
 foreach($data[1] as $list){
 ?>
-<a href="?action=itemView&id=<? echo $list['list_id']?>">
 <tr>
 <th align=left><? echo $list["name"]?></th>
 <th align=left><? echo $list["event"]?></th>
@@ -12,10 +11,10 @@ foreach($data[1] as $list){
 <th align=left><? echo $list["city"]?></th>
 <th align=left><? echo $list["state"]?></th>
 <th align=left><? echo $list["zip"]?></th>
-<th align=left ><a class="button" href="?action=updateListButton&id=<? echo $list['list_id']?>">Edit List</a></th>
-<th align=left><a class="button" href="?action=deleteListAction&id=<? echo $list['list_id']?>">Delete List</a></th>
+<th align=left><a href="?action=updateListButton&id=<? echo $list['list_id']?>">Edit List</a></th>
+<th align=left><a href="?action=deleteListAction&id=<? echo $list['list_id']?>">Delete List</a></th>
+<th align=left><a href="?action=itemView&id=<? echo $list['list_id']?>">View Items</a></th>
 </tr>
-</a>
 <?
 }
 ?>
