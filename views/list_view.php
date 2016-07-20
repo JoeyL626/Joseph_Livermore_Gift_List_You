fee450ea590f034e6a37eac6342bd219bad81f42
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 14:35:24 -0400

    style

--- views/list_view.php
+++ views/list_view.php
@@ -5,8 +5,8 @@
 foreach($data[1] as $list){
 ?>
 <tr>
-<td id="eventLength"><? echo $list["event"]?></td>
-<td id="addressLength" ><? echo $list["address"]." ".$list["city"].", ".$list["state"]." ".$list["zip"]?></td>
+<td width="20%"><? echo $list["event"]?></td>
+<td width="40%" ><? echo $list["address"]." ".$list["city"].", ".$list["state"]." ".$list["zip"]?></td>
 <td align=left><a class="button listButton" href="?action=updateListButton&id=<? echo $list['list_id']?>">Edit</a></td>
 <td align=left><a class="button listButton" href="?action=itemView&id=<? echo $list['list_id']?>">Items</a></td>
 </tr>
