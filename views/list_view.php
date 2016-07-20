054b8c9af7e4f206cf1f5f101aa62ebc3068d54b
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 14:54:19 -0400

    style

--- views/list_view.php
+++ views/list_view.php
@@ -1,18 +1,20 @@
 
 <div class="small-5 columns float-right">
 <table >
-<th width="150">Event</th>
-<th width="500">Address</th>
-<th width="150">Edit</th>
-<th width="150">Items</th>
+<tr>
+<th width="250">Event</th>
+<th width="400">Address</th>
+<th width="50">Edit</th>
+<th width="50">Items</th>
+</tr>
 <?
 foreach($data[1] as $list){
 ?>
 <tr>
 <td><? echo $list["event"]?></td>
 <td><? echo $list["address"]." ".$list["city"].", ".$list["state"]." ".$list["zip"]?></td>
-<td align=left><a class="button listButton" href="?action=updateListButton&id=<? echo $list['list_id']?>">Edit</a></td>
-<td align=left><a class="button listButton" href="?action=itemView&id=<? echo $list['list_id']?>">Items</a></td>
+<td><a class="button listButton" href="?action=updateListButton&id=<? echo $list['list_id']?>">Edit</a></td>
+<td><a class="button listButton" href="?action=itemView&id=<? echo $list['list_id']?>">Items</a></td>
 </tr>
 <?
 }
