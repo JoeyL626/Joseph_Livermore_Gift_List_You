3790391dc6656aeda5536d20899bb14b614c7993
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 11:09:02 -0400

    styles

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -1,10 +1,10 @@
 
 
 <ul class="menu align-right">
-<li><a href="?action=profile">Profile</a></li>
-<li><a href="?action=updateUserButton">Edit Profile</a></li>
-<li><a href="?action=addItemButton">Add Item</a></li>
-<li><a href="?action=logout">Logout</a></li>
+<li><a class="button" href="?action=profile">Profile</a></li>
+<li><a class="button" href="?action=updateUserButton">Edit Profile</a></li>
+<li><a class="button" href="?action=addItemButton">Add Item</a></li>
+<li><a class="button" href="?action=logout">Logout</a></li>
 </ul>
 </div>
 </div>
--- views/user_login.php
+++ views/user_login.php
@@ -4,9 +4,9 @@
 <form enctype="multipart/form-data" action="?action=loginAction" method="POST">
 	<fieldset>
 
-		Email :<input type="text" name="email" value="" placeholder="email" /><br>
+		<input type="text" name="email" value="" placeholder="email" /><br>
 
-		Password: <input type="password" name="password" value="" placeholder="password"/><br>
+		<input type="password" name="password" value="" placeholder="password"/><br>
 
     <input type="submit" name="submit" value="Login"/>
 
