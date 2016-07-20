45cd6c181510acc121f7196a1db468b3342dba6a
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 03:21:10 -0400

    nav stuff

--- views/chrome_page_navigation.php
+++ views/chrome_page_navigation.php
@@ -1,8 +1,8 @@
 
-	<ul class="dropdown menu-centered" data-dropdown-menu>
+	<ul class="dropdown menu align-centered" data-dropdown-menu>
 	  <li>
 		  <a>Menu</a>
-		   <ul class="menu-centered">
+		   <ul class="menu align-centered">
 					<li><a href="?action=chromeAddListView">Add List</a></li>
 					<li><a href="?action=chromeAddItemView">Add Item</a></li>
 					<li><a href="?action=chromeLogout">Logout</a></li>
