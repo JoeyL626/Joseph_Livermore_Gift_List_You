ff59c14b133ae39a3517e0c8eb1d87ed50b61413
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 06:00:50 -0400

    profile grid

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -1,9 +1,9 @@
 
 
 <ul class="menu align-right">
-<a href="?action=updateUserButton">Edit Profile?</a>
-<a href="?action=deleteUserButton">Delete Account?</a>
-<a href="?action=addItemButton">Add Item</a>
+<li><a href="?action=updateUserButton">Edit Profile?</a></li>
+<li><a href="?action=deleteUserButton">Delete Account?</a></li>
+<li><a href="?action=addItemButton">Add Item</a></li>
 <li><a href="?action=logout">Logout</a></li>
 </ul>
 </div>
