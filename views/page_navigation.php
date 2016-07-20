e551ac021a64ab3c3bdffba1c8cbb20805d20b50
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 11:33:05 -0400

    nav stuff

--- views/page_navigation.php
+++ views/page_navigation.php
@@ -1,4 +1,4 @@
-<div class="large-6 small-4 small-centered columns">
+
 <form enctype="multipart/form-data" action="?action=loginAction" method="POST">
 	<fieldset>
 		
@@ -22,7 +22,6 @@
 
   </fieldset>
 </form>
-</div>
 </div>
 
 
