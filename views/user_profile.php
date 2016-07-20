523ef2f02239612c7972d84a34eae3b5519d758b
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 17:15:17 -0400

    style

--- views/user_profile.php
+++ views/user_profile.php
@@ -1,6 +1,6 @@
-<div class="row">
+<div class="row" style="padding-left: 1rem;">
 <div class="row float-left"  >
-<div class="small-5 columns" style="width:100%; padding-left: 1.5rem;">
+<div class="small-5 columns" style="width:100%; padding-left: 1.4rem;">
 <table >
 <tr>
 <th>Name</th>
