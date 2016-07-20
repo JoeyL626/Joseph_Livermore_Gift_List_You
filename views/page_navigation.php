c6d7e4e2635a973f5bb60de5af4b8c969a5605dc
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 03:08:55 -0400

    nav try

--- views/page_navigation.php
+++ views/page_navigation.php
@@ -1,10 +1,12 @@
-
-<nav>
 <ul class="dropdown menu" data-dropdown-menu>
+	  <li>
+		  <a href="#">Menu</a>
+		   <ul class="menu">
 <li><a href="?action=signup">Sign Up</a></li>
 <li><a href="?action=login">Login</a></li>
 <li><a href="?action=logout">Logout</a></li>
 </ul>
-</nav>
+</li>
+</ul>
 </header>
 <br>
