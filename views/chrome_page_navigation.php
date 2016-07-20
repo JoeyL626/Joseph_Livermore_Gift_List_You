a6ff91e1c5aed12dab3295ca7dcff46261bfceed
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 03:13:44 -0400

    navigation stuff

--- views/chrome_page_navigation.php
+++ views/chrome_page_navigation.php
@@ -1,13 +1,8 @@
 
-	<ul class="dropdown menu" data-dropdown-menu>
-	  <li>
-		  <a href="#">Menu</a>
-		   <ul class="menu">
-					<li><a href="?action=chromeAddListView">Add List</a></li>
-					<li><a href="?action=chromeAddItemView">Add Item</a></li>
-					<li><a href="?action=chromeLogout">Logout</a></li>
-				</ul>
-		</li>
-	</ul>
+<ul class="dropdown menu" data-dropdown-menu>
+<li><a href="?action=chromeAddListView">Add List</a></li>
+<li><a href="?action=chromeAddItemView">Add Item</a></li>
+<li><a href="?action=chromeLogout">Logout</a></li>
+</ul>
 </header>
 <br>
