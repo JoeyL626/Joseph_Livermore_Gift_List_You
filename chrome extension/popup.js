016451be0a62d0d23da8e4d3558685ef5e59f3d5
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-19 20:27:46 -0400

    chrome try again

--- chrome extension/popup.js
+++ chrome extension/popup.js
@@ -1,7 +1,5 @@
 chrome.tabs.getSelected(null, function(tab) {
 
-    var iframe = document.createElement('iframe');
-		iframe.src = "http://gift-list-you.herokuapp.com/?action=chromeAddItemView&url="+tab.url;
-
-		$("#host").appendChild(iframe);
+    document.getElementById('webAddress').setAttribute('src', 'http://gift-list-you.herokuapp.com/?action=chromeAddItemView&url='+tab.url);
+    
 });
