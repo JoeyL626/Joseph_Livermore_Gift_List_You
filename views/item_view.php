8be321edc2e1a60ce091651f460abeffc033d522
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 17:55:00 -0400

    styling

--- views/item_view.php
+++ views/item_view.php
@@ -7,13 +7,14 @@
 			foreach($data[1] as $item){
 			?>
 				<li id="<?php echo $item['item_id']; ?>">
-				<span></span>
+				<span>
 				<div>
 				<? echo $item["name"]?>
 				<? echo $item["url"]?>
 				<? echo $item["price"]?>
 				<a href="?action=updateItemButton&id=<? echo $item['item_id']?>">Edit Item</a>
 				<a href="?action=deleteItemAction&id=<? echo $item['item_id']?>&list_id=<? echo $item['list_id']?>">Delete Item</a></div>
+				</span>
 				</li>	
 			<?
 			}
