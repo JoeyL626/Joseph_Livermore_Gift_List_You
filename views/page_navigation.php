93b0ac883727409a265e4c52e893c9451978bf25
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-07-20 03:24:01 -0400

    nav stuff

--- views/page_navigation.php
+++ views/page_navigation.php
@@ -1,6 +1,6 @@
 
 <nav>
-<ul class="menu-centered">
+<ul class="menu-centered" style="list-style: none;">
 <li><a href="?action=signup">Sign Up</a></li>
 <li><a href="?action=login">Login</a></li>
 <li><a href="?action=logout">Logout</a></li>
