b97eb5a9ddc4f61473d54dc82788a942c699f641
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-18 21:40:21 -0400

    Facebook taken out for now

--- views/user_login.php
+++ views/user_login.php
@@ -13,18 +13,18 @@
   </fieldset>
 </form>
 <?
-$fb = new Facebook\Facebook([
-  'app_id' => '{1766483266905544}', // Replace {app-id} with your app id
-  'app_secret' => '{67a267e941fbd2f9b4985b4c133488ea}',
-  'default_graph_version' => 'v2.2',
-  ]);
+// $fb = new Facebook\Facebook([
+//   'app_id' => '{1766483266905544}', // Replace {app-id} with your app id
+//   'app_secret' => '{67a267e941fbd2f9b4985b4c133488ea}',
+//   'default_graph_version' => 'v2.2',
+//   ]);
 
-$helper = $fb->getRedirectLoginHelper();
+// $helper = $fb->getRedirectLoginHelper();
 
-$permissions = ['email']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('http://gift-list-you.herokuapp.com/fbCallback.php', $permissions);
+// $permissions = ['email']; // Optional permissions
+// $loginUrl = $helper->getLoginUrl('http://gift-list-you.herokuapp.com/fbCallback.php', $permissions);
 
-echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+// echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 ?>
 </div>
 </div>
