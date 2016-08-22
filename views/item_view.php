0cfbcb5fad737ff8749ae4d595d0f34c1264c278
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 02:28:56 -0400

    style

--- views/item_view.php
+++ views/item_view.php
@@ -22,7 +22,7 @@
 				<div class="row show-for-small-only">
 				<div class="small-7 small-centered columns" >
 				<a class="button" href="?action=updateItemButton&id=<? echo $item['item_id']?>">Edit Item</a>
-				<a class="button" href="?action=deleteItemAction&id=<? echo $item['item_id']?>&list_id=<? echo $item['list_id']?>">Delete Item</a>
+				<a class="button float-right" href="?action=deleteItemAction&id=<? echo $item['item_id']?>&list_id=<? echo $item['list_id']?>">Delete Item</a>
 				</div>
 				</div>
 				</span>
