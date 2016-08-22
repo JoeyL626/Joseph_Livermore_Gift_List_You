095652c1d90047587cc403315175335fb7ad70c3
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 05:52:57 -0400

    style

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -5,7 +5,7 @@
 		<h1> Gift List You </h1>
 	
 	</div>
-<div class="medium-6 columns show-for-large-only">
+<div class="medium-6 columns hide-for-medium">
 <ul class="menu align-right">
 <li><a class="button" href="?action=profile">Profile</a></li>
 <li><a class="button" href="?action=updateUserButton">Edit Profile</a></li>
