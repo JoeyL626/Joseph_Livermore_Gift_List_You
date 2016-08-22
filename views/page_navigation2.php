9500d057be86b838bc48ea3b40e8102442bc3a4e
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 06:43:13 -0400

    style

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -1,40 +1,19 @@
 <div id="header">
-<div class="row">
-  <div class="medium-7 small-9 small-centered columns">
-
-		<h1> Gift List You </h1>
+	<div class="row">
+	  <div class="medium-7 small-9 small-centered columns">
 	
+			<h1> Gift List You </h1>
+		
+		</div>
+		</div>
+	<div class="row">
+		<div class="small-8 small-centered columns">
+			<ul class="menu align-right">
+				<li><a class="button" href="?action=profile">Profile</a></li>
+				<li><a class="button" href="?action=updateUserButton">Edit Profile</a></li>
+				<li><a class="button" href="?action=addItemButton">Add Item</a></li>
+				<li><a class="button" href="?action=logout">Logout</a></li>
+			</ul>
+		</div>
 	</div>
-<div class="medium-6 columns show-for-large">
-<ul class="menu align-right">
-<li><a class="button" href="?action=profile">Profile</a></li>
-<li><a class="button" href="?action=updateUserButton">Edit Profile</a></li>
-<li><a class="button" href="?action=addItemButton">Add Item</a></li>
-<li><a class="button" href="?action=logout">Logout</a></li>
-</ul>
-</div>
-</div>
-
-
-<div class="row show-for-medium-only">
-<div class="medium-7 medium-centered columns">
-<ul class="menu align-right">
-<li><a class="button" href="?action=profile">Profile</a></li>
-<li><a class="button" href="?action=updateUserButton">Edit Profile</a></li>
-<li><a class="button" href="?action=addItemButton">Add Item</a></li>
-<li><a class="button" href="?action=logout">Logout</a></li>
-</ul>
-</div>
-</div>
-
-<div class="row show-for-small-only">
-<div class="medium-7  medium-centered columns">
-<ul class="menu align-right">
-<li><a class="button" href="?action=profile">Profile</a></li>
-<li><a class="button" href="?action=updateUserButton">Edit Profile</a></li>
-<li><a class="button" href="?action=addItemButton">Add Item</a></li>
-<li><a class="button" href="?action=logout">Logout</a></li>
-</ul>
-</div>
-</div>
 </div>
