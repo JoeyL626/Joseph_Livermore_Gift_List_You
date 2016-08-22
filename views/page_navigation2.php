a2a0b6b2e25798915a67acb95b33579856537f18
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 08:46:35 -0400

    style

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -1,13 +1,13 @@
 <div id="header">
 	<div class="row">
-	  <div class="medium-7 small-12 small-centered columns">
+	  <div class="medium-7 small-10 small-centered columns">
 	
 			<h1> Gift List You </h1>
 		
 		</div>
 		</div>
 	<div class="row">
-		<div class="large-6 medium-8 small-12 small-centered columns">
+		<div class="large-6 medium-8 small-10 small-centered columns">
 			<ul class="menu align-right">
 				<li><a class="button" href="?action=profile">Profile</a></li>
 				<li><a class="button" href="?action=updateUserButton">Edit Profile</a></li>
