6dac180690a6571e36200b22b682b103149b5b14
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 01:14:55 -0400

    style

--- views/item_view.php
+++ views/item_view.php
@@ -22,7 +22,7 @@
 				</div>
 				</div>
 				<div class="row show-for-small-only">
-				<div class="small-4 small-offset-4 columns" >
+				<div class="small-12 columns" >
 				<a class="button" href="?action=updateItemButton&id=<? echo $item['item_id']?>">Edit Item</a>
 				<a class="button" href="?action=deleteItemAction&id=<? echo $item['item_id']?>&list_id=<? echo $item['list_id']?>">Delete Item</a>
 				</div>
