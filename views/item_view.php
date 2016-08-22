76f95942c1b3d126bce4269f8febff3b5ac9258c
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 00:24:20 -0400

    style

--- views/item_view.php
+++ views/item_view.php
@@ -15,7 +15,7 @@
 				<div class="large-2 columns" >
 				<? echo $item["price"]?>
 				</div>
-				<div class="large-3 columns" >
+				<div class="large-5 columns" >
 				<a class="button" href="?action=updateItemButton&id=<? echo $item['item_id']?>">Edit Item</a>
 				<a class="button" href="?action=deleteItemAction&id=<? echo $item['item_id']?>&list_id=<? echo $item['list_id']?>">Delete Item</a>
 				</div>
