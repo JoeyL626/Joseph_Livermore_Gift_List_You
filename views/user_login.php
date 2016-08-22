245ceb50b54958e87bd8cd095e1e2b1938c25033
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 04:13:31 -0400

    style

--- views/user_login.php
+++ views/user_login.php
@@ -1,6 +1,6 @@
 
 <div class="row show-for-small-only">
-<div class="large-6 small-4 small-centered columns">
+<div class="large-6 small-8 small-centered columns">
 <form enctype="multipart/form-data" action="?action=loginAction" method="POST">
 	<fieldset>
 
@@ -12,19 +12,5 @@
 
   </fieldset>
 </form>
-<?
-// $fb = new Facebook\Facebook([
-//   'app_id' => '{1766483266905544}', // Replace {app-id} with your app id
-//   'app_secret' => '{67a267e941fbd2f9b4985b4c133488ea}',
-//   'default_graph_version' => 'v2.2',
-//   ]);
-
-// $helper = $fb->getRedirectLoginHelper();
-
-// $permissions = ['email']; // Optional permissions
-// $loginUrl = $helper->getLoginUrl('http://gift-list-you.herokuapp.com/fbCallback.php', $permissions);
-
-// echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
-?>
 </div>
 </div>
