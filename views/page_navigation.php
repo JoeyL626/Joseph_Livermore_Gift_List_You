c7977158c7aea5465686a6a73f5b13d353b35bfa
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 04:41:37 -0400

    style

--- views/page_navigation.php
+++ views/page_navigation.php
@@ -31,14 +31,14 @@
 
 <div id="header" class="show-for-small-only">
 <div class="row">
-  <div class="large-6 columns">
+  <div class="small-12 columns">
 
 		<h1> Gift List You </h1>
 	
 	</div>
 	</div>
 	<div class="row">
-	<div class="small-6 columns show-for-small-only" id="header">
+	<div class="small-12 columns show-for-small-only" id="header">
 		<ul class="menu-center" style="list-style: none;">
 			<li><a class="button" href="?action=signup">Signup</a></li>
 			<li><a class="button" href="?action=login">Login</a></li>
