c0b70d661cf5601687f577b23ada5595913e209f
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 05:39:15 -0400

    style

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -14,4 +14,25 @@
 </div>
 </div>
 
+<div class="row show-for-medium-only">
+<div class="medium-6 medium-centered columns">
+<ul class="menu align-right">
+<li><a class="button" href="?action=profile">Profile</a></li>
+<li><a class="button" href="?action=updateUserButton">Edit Profile</a></li>
+<li><a class="button" href="?action=addItemButton">Add Item</a></li>
+<li><a class="button" href="?action=logout">Logout</a></li>
+</ul>
+</div>
+</div>
+
+<div class="row show-for-small-only">
+<div class="medium-6  medium-centered columns">
+<ul class="menu align-right">
+<li><a class="button" href="?action=profile">Profile</a></li>
+<li><a class="button" href="?action=updateUserButton">Edit Profile</a></li>
+<li><a class="button" href="?action=addItemButton">Add Item</a></li>
+<li><a class="button" href="?action=logout">Logout</a></li>
+</ul>
+</div>
+</div>
 
