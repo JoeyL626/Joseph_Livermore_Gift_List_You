3a31e2d6c66bd7a3fcf9b6c5b58bc14525b96a23
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 06:57:06 -0400

    style

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -7,7 +7,7 @@
 		</div>
 		</div>
 	<div class="row">
-		<div class="medium-8 small-12 small-centered columns">
+		<div class="large-6 medium-8 small-12 small-centered columns">
 			<ul class="menu align-right">
 				<li><a class="button" href="?action=profile">Profile</a></li>
 				<li><a class="button" href="?action=updateUserButton">Edit Profile</a></li>
