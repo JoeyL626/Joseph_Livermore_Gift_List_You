0e641dda83b923a6277dbdf6a4ec93590cf8217d
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 02:25:09 -0400

    style

--- views/item_view.php
+++ views/item_view.php
@@ -6,7 +6,6 @@
 			?>
 				<li id="<?php echo $item['item_id']; ?>">
 				<span>
-				<button>
 				<div class="medium-2 small-6 columns" >
 				<? echo $item["name"]?>
 				</div>
@@ -26,7 +25,6 @@
 				<a class="button" href="?action=deleteItemAction&id=<? echo $item['item_id']?>&list_id=<? echo $item['list_id']?>">Delete Item</a>
 				</div>
 				</div>
-				</button>
 				</span>
 				</li>	
 			<?
