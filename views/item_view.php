beca5661cab0d1c8602b50ca02c71d02e58c3e3e
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 00:37:15 -0400

    style

--- views/item_view.php
+++ views/item_view.php
@@ -6,16 +6,16 @@
 			?>
 				<li id="<?php echo $item['item_id']; ?>">
 				<span>
-				<div class="small-2 columns" >
+				<div class="medium-2 columns" >
 				<? echo $item["name"]?>
 				</div>
-				<div class="small-4 columns" >
+				<div class="medium-4 columns" >
 				<? echo $item["url"]?>
 				</div>
-				<div class="small-2 columns" >
+				<div class="medium-2 columns" >
 				<? echo $item["price"]?>
 				</div>
-				<div class="small-4 columns" >
+				<div class="medium-4 columns" >
 				<a class="button" href="?action=updateItemButton&id=<? echo $item['item_id']?>">Edit Item</a>
 				<a class="button" href="?action=deleteItemAction&id=<? echo $item['item_id']?>&list_id=<? echo $item['list_id']?>">Delete Item</a>
 				</div>
