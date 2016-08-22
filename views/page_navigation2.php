6a1923a824c3b5bd7f890757433552623e85be0e
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 17:40:25 -0400

    style

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -9,9 +9,9 @@
 	<div class="row">
 		<div class="large-4 medium-6 small-10 small-centered columns">
 			<ul class="menu">
-				<li><a class="button" href="?action=profile">Profile</a></li>
-				<li><a class="button" href="?action=addItemButton">Add Item</a></li>
-				<li><a class="button" href="?action=logout">Logout</a></li>
+				<li><a class="large button" href="?action=profile">Profile</a></li>
+				<li><a class="large button" href="?action=addItemButton">Add Item</a></li>
+				<li><a class="large button" href="?action=logout">Logout</a></li>
 			</ul>
 		</div>
 	</div>
