564134e38746637fafc3959f162de0b4d2c5fd9a
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 19:44:57 -0400

    style

--- views/list_add_medium.php
+++ views/list_add_medium.php
@@ -1,7 +1,7 @@
 <div class="row show-for-medium-only">
 <div class="medium-12 columns">
 <div style="background-color: #0C56AF;">
-<h1 style="margin-left: 200px;">Create List</h1>
+<h1 style="margin-left: 400px;">Create List</h1>
 <form enctype="multipart/form-data" action="?action=addListAction" method="POST">
 	<fieldset>
 
