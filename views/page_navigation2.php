426d852247915e71df9f88b8871308d0d051e342
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-23 05:39:00 -0400

    style

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -7,7 +7,7 @@
 		</div>
 		</div>
 	<div class="row">
-		<div class="small-12 small-centered columns">
+		<div class="small-11 small-centered columns">
 			<ul class="menu">
 				<li><a style="margin:0 0 5px 0;" class="large button" href="?action=profile">Profile</a></li>
 				<li><a style="margin:0 5px 5px 5px;" class="large button" href="?action=addItemButton">Add Item</a></li>
