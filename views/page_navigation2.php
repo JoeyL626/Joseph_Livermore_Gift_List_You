3232d08a71ce658380e4637f9389290f65e2008b
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-23 05:45:50 -0400

    style

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -1,4 +1,22 @@
-<div id="header">
+<div id="header" class="hide-for-small-only">
+	<div class="row">
+	  <div class="medium-7 columns">
+	
+			<h1 style="font-family: 'Rochester', cursive;"> Gift List You </h1>
+		
+		</div>
+		<div class="medium-5 columns">
+			<ul class="menu">
+				<li><a style="margin:0 0 5px 0;" class="large button" href="?action=profile">Profile</a></li>
+				<li><a style="margin:0 5px 5px 5px;" class="large button" href="?action=addItemButton">Add Item</a></li>
+				<li><a style="margin:0 0 5px 0;" class="large button" href="?action=logout">Logout</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+
+<div id="header" class="show-for-small-only">
 	<div class="row">
 	  <div class="small-8 small-centered columns">
 	
