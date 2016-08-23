5e42343bc0f3c353039721e6c90ef8197bdd203f
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-23 12:47:20 -0400

    style

--- views/share_navigation.php
+++ views/share_navigation.php
@@ -1,7 +1,7 @@
 <div id="header" class="row">
   <div class="medium-4 small-5 small-centered columns">
 
-		<h2 style="font-family: 'Rochester', cursive;"> Gift List You </h2>
+		<h2 style="font-family: 'Rochester', cursive; font-size: 40px;"> Gift List You </h2>
 	
 	</div>
 	
