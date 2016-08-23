588169c1350010a278560b37c9d3e977891a9d57
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-23 10:52:04 -0400

    style

--- views/user_delete.php
+++ views/user_delete.php
@@ -1,6 +1,6 @@
-<div style="background-color: #2196F3; border-top-right-radius:15px; border-top-left-radius:15px;">
 <div class="row">
 <div class="large-6 small-4 small-centered columns">
+<div style="background-color: #2196F3; border-top-right-radius:15px; border-top-left-radius:15px;">
 <form action="?action=deleteUserAction" align=center method="POST">
 		<h3 style="font-family: 'Rochester', cursive;">Area you sure you want to delete you account?</h3><br>
 		<h3 style="font-family: 'Rochester', cursive;">This is permanent.</h3>
