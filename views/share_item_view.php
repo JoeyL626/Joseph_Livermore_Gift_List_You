cd2e867625865c0f6b13cde653511732b6a7f331
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-23 12:19:42 -0400

    style

--- views/share_item_view.php
+++ views/share_item_view.php
@@ -1,4 +1,23 @@
-<div class="row">
+
+<div class="medium-7 columns hide-for-small-only">
+<table>
+<?
+foreach($data[0] as $list){
+?>
+<tr>
+<td><? echo $list["name"]?></td>
+<td><? echo $list["url"]?></td>
+<td><? echo $list["price"]?></td>
+</tr>
+<?
+}
+?>
+</table>
+</div>
+</div>
+
+
+<div class="row show-for-small-only">
 <div class="small-8 small-centered columns">
 <table>
 <?
