4c1870b3ee90e4a16711f71b362ada238cc956e6
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-23 08:33:07 -0400

    style

--- views/page_navigation.php
+++ views/page_navigation.php
@@ -32,7 +32,7 @@
 
 <div id="header" class="show-for-small-only">
 <div class="row">
-  <div class="small-6 small-centered columns">
+  <div class="small-7 small-centered columns">
 
 		<h1 style="font-family: 'Rochester', cursive; font-size: 40px;"> Gift List You </h1>
 	
