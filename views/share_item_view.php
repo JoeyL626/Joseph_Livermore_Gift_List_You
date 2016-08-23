367460d0a92a704e94cbfebc31f36eac0301e15b
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-23 12:35:08 -0400

    style

--- views/share_item_view.php
+++ views/share_item_view.php
@@ -1,8 +1,10 @@
 
 <div class="medium-7 columns hide-for-small-only">
+<div style="background-color: #2196F3; border-top-right-radius:15px; border-top-left-radius:15px;">
 <div class="row">
 <div class="large-6 small-4 small-centered columns">
 <h2 style="font-family: 'Rochester', cursive;">Recipient List</h2>
+</div>
 </div>
 </div>
 <table>
