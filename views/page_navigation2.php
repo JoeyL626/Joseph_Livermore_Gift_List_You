d515b8341b77943bc83d03d151e41360e8932a77
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-23 05:02:58 -0400

    style

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -1,13 +1,13 @@
 <div id="header">
 	<div class="row">
-	  <div class="medium-8 small-10 small-centered columns">
+	  <div class="medium-4 small-10 small-centered columns">
 	
 			<h1 style="font-family: 'Rochester', cursive;"> Gift List You </h1>
 		
 		</div>
 		</div>
 	<div class="row">
-		<div class="large-5 medium-7 small-11 small-centered columns">
+		<div class="large-5 medium-7 small-11 medium-offset-6 small-centered columns">
 			<ul class="menu">
 				<li><a style="margin:0 5px 5px 5px;" class="large button" href="?action=profile">Profile</a></li>
 				<li><a style="margin:0 5px 5px 5px;" class="large button" href="?action=addItemButton">Add Item</a></li>
