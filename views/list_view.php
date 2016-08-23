c9d02b80ba6554b8246389223c63bf83582f035a
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-23 03:35:40 -0400

    style

--- views/list_view.php
+++ views/list_view.php
@@ -1,6 +1,6 @@
 
 <div class="large-7 medium-12 columns">
-<div style="background-color: #0C56AF; border-top-right-radius:15px; border-top-left-radius:15px;">
+<div style="background-color: #2196F3; border-top-right-radius:15px; border-top-left-radius:15px;">
 <div class="row">
 <div class="large-6 small-5 small-centered columns">
 <h2>Your Lists</h2>
