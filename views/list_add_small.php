bae7bc4223140cb0a6eae256cbaf7391820b3582
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-22 20:01:15 -0400

    style

--- views/list_add_small.php
+++ views/list_add_small.php
@@ -2,7 +2,7 @@
 <div class="small-12 columns">
 <div style="background-color: #0C56AF;">
 <div class="row">
-<div class="small-6 small-centered columns">
+<div class="small-5 small-centered columns">
 <h1>Create List</h1>
 </div>
 </div>
