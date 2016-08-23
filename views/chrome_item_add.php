b46a2b2093560afcae82c48f4375564a6c1f378e
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-23 11:16:24 -0400

    style

--- views/chrome_item_add.php
+++ views/chrome_item_add.php
@@ -19,7 +19,7 @@
 								</select><br>
   	
 <div class="row">
-<div class="medium-3 medium-centered columns">
+<div class="small-3 small-centered columns">
   	<input class="button" type="submit" name="submit" value="Add Item"/>
   	</div>
   	</div>
--- views/chrome_list_add.php
+++ views/chrome_list_add.php
@@ -75,7 +75,7 @@
 		<input type="text" name="zip" value="" placeholder="Zip" />
 	
 	<div class="row">
-<div class="medium-3 medium-centered columns">
+<div class="small-3 small-centered columns">
   	<input class="button" type="submit" name="submit" value="Create List"/>
   	</div>
   	</div>
