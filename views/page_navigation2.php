f67835a702cee0a82192c0073f0d89061d24156a
joey livermore <joeylivermore@Joeys-MacBook-Pro-5.local>
2016-08-23 09:11:30 -0400

    style

--- views/page_navigation2.php
+++ views/page_navigation2.php
@@ -1,7 +1,6 @@
 <div class="hide-for-small-only">
 	<div class="row">
-	  <div class="large-7 medium-6 columns">
-	  <div id="header">
+	  <div id="header" class="large-7 medium-6 columns">
 	
 			<h1 style="font-family: 'Rochester', cursive; font-size: 50px;"> Gift List You </h1>
 		
@@ -12,27 +11,26 @@
 				<li><a style="margin:15px 5px 5px 5px;" class="large button" href="?action=addItemButton">Add Item</a></li>
 				<li><a style="margin:15px 0 5px 0;" class="large button" href="?action=logout">Logout</a></li>
 			</ul>
-			</div>
 		</div>
 	</div>
+</div>
 
 
 <div class="show-for-small-only">
 	<div class="row">
-	  <div class="small-6 small-centered columns">
-	  <div id="header">
+	  <div id="header" class="small-6 small-centered columns">
 	
 			<h1 style="font-family: 'Rochester', cursive; font-size: 30px;"> Gift List You </h1>
 		
 		</div>
 		</div>
 	<div class="row">
-		<div class="small-9 small-centered columns">
+		<div id="header" class="small-9 small-centered columns">
 			<ul class="menu">
 				<li><a style="margin:0 0 5px 0;" class="button" href="?action=profile">Profile</a></li>
 				<li><a style="margin:0 5px 5px 5px;" class="button" href="?action=addItemButton">Add Item</a></li>
 				<li><a style="margin:0 0 5px 0;" class="button" href="?action=logout">Logout</a></li>
 			</ul>
-			</div>
 		</div>
 	</div>
+</div>
